64f26cf152a76eca5f2fddd6ffc20f9271e096f0
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-25 15:15:55 +0600

    Url end poing for contact and post

--- blog-node/app.js
+++ blog-node/app.js
@@ -13,6 +13,14 @@ app.get('/about', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/about.html'));
 });
 
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/contact.html'));
+});
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/post.html'));
+});
+
 app.listen(3000, () => {
     console.log('App listening on port 3000');
 });
