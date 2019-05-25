4806fc12787a750e9947aadbce93263ac1a765c4
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-25 15:12:01 +0600

    Url endpoint for /about

--- blog-node/app.js
+++ blog-node/app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/about.html'));
+});
+
 app.listen(3000, () => {
     console.log('App listening on port 3000');
 });
