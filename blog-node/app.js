503db2a9470999dcfe7883e848eaa73e462d10d1
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-25 15:03:27 +0600

    API for home page and theme folder from bootstrap template

--- blog-node/app.js
+++ blog-node/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = new express();
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/index.html'));
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000');
