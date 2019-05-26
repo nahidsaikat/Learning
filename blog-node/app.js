1f81cf9020f2b5cd46cfc7c2e24cd10624dab1f9
nahidsaikat <nahidsaikatft40@gmail.com>
2019-05-27 01:45:59 +0600

    Storing session in mongo db

--- blog-node/app.js
+++ blog-node/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
  
 const createPostController = require('./controllers/createPost');
 const homePageController = require('./controllers/homePage');
@@ -16,14 +17,19 @@ const loginUserController = require('./controllers/loginUser');
  
 const app = new express();
  
-app.use(expressSession({
-  secret: 'secret'
-}));
-
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch((err) => console.error('Something went wrong', err));
  
+const mongoStore = connectMongo(expressSession);
+ 
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
+
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(expressEdge);
