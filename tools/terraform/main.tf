e5c9da3294b62dc1b2b971fd2e025c30fefec9e5
nahidsaikat <nahidsaikatft40@gmail.com>
2020-04-06 23:39:31 +0600

    Add aws sqs resources in terraform

--- tools/terraform/main.tf
+++ tools/terraform/main.tf
@@ -10,12 +10,31 @@ provider "aws" {
   endpoints {
     s3 = "http://localhost:4572"
     ec2 = "http://localhost:4597"
+    sqs = "http://localhost:4576"
   }
 }
 
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
   acl    = "public-read"
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "terraform-example-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_sqs_queue" "terraform_queue_fifo" {
+  name                        = "terraform-example-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
 }
 
 # resource "aws_instance" "example" {
