bedd7138d97d538766764285c40e935c262fc5de
nahidsaikat <nahidsaikatft40@gmail.com>
2021-02-21 12:11:30 +0600

    Add flask mail in app instance

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -3,6 +3,7 @@ import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
