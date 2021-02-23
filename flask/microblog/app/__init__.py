86eea0c87b7eac882c09a260dffe4058f343259a
nahidsaikat <nahidsaikatft40@gmail.com>
2021-02-23 07:17:01 +0600

    Add flask bootstrap framework in microblog.

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -3,6 +3,7 @@ import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -17,6 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
