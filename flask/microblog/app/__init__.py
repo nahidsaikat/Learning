8445e3754cce320c5d2eb9f25df5cf65e77c6074
nahidsaikat <nahidsaikatft40@gmail.com>
2021-02-26 12:45:01 +0600

    Integrate flask translation feature in microblog

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -2,13 +2,15 @@ import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_babel import lazy_gettext as _l
 
 from .config import Config
 
@@ -18,9 +20,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
 
@@ -52,3 +56,8 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
--- flask/microblog/app/routes.py
+++ flask/microblog/app/routes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from flask import render_template, flash, redirect, url_for, request
+from flask_babel import get_locale
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
@@ -16,6 +17,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])
