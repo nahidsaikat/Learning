7dc21567fce60826d30e84828e80dae9545fa164
nahidsaikat <nahidsaikatft40@gmail.com>
2021-02-27 19:55:10 +0600

    Add translation service to microblog

--- flask/microblog/app/routes.py
+++ flask/microblog/app/routes.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
+from flask import jsonify
 from flask import render_template, flash, redirect, url_for, request
 from flask_babel import get_locale
 from flask_login import current_user, login_user, logout_user, login_required
+from guess_language import guess_language
 from werkzeug.urls import url_parse
 
 from app import app, db
@@ -10,6 +12,7 @@ from app.email import send_password_reset_email
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, \
     ResetPasswordForm
 from app.models import User, Post
+from app.translate import translate
 
 
 @app.before_request
@@ -26,7 +29,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
@@ -198,3 +205,11 @@ def reset_password(token):
         flash('Your password has been reset.')
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
