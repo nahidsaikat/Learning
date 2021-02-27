7271f21c7d660e7c95cad15567d0eb9ad897940b
nahidsaikat <nahidsaikatft40@gmail.com>
2021-02-27 19:57:03 +0600

    Import g in routes of microblog

--- flask/microblog/app/routes.py
+++ flask/microblog/app/routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import jsonify
+from flask import jsonify, g
 from flask import render_template, flash, redirect, url_for, request
 from flask_babel import get_locale
 from flask_login import current_user, login_user, logout_user, login_required
