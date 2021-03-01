b1a6709ea9acfa3b9601b753c08cfac234990c35
nahidsaikat <nahidsaikatft40@gmail.com>
2021-03-01 07:10:16 +0600

    Add errors blueprint to microblog

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -26,7 +26,10 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
-from app import routes, models, errors
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app import routes, models
 
 
 if not app.debug:
