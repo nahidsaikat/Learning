465f88193387e687fa9972f4e300e3e1d166fdcf
nahidsaikat <nahidsaikatft40@gmail.com>
2021-03-10 06:35:06 +0600

    Add search endpoint to microblog.

--- flask/microblog/app/main/forms.py
+++ flask/microblog/app/main/forms.py
@@ -1,4 +1,6 @@
+from flask import request
 from flask_wtf import FlaskForm
+from flask_babel import Babel, lazy_gettext as _l
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Length
 
@@ -29,3 +31,14 @@ class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
