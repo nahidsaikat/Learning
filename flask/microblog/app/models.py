17d44214d0461971eac52a80551835aac806f38d
nahidsaikat <nahidsaikatft40@gmail.com>
2021-03-22 07:40:17 +0600

    Add token field to User model with supporting methods.

--- flask/microblog/app/models.py
+++ flask/microblog/app/models.py
@@ -1,5 +1,7 @@
+import base64
 import json
-from datetime import datetime
+import os
+from datetime import datetime, timedelta
 from hashlib import md5
 from time import time
 
@@ -112,6 +114,9 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                                     lazy='dynamic')
     tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -211,6 +216,25 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 
 @login.user_loader
