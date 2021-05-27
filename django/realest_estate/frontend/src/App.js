065785874608cf3d9943717cb6170f132bec8797
nahidsaikat <nahidsaikatft40@gmail.com>
2021-05-27 22:06:06 +0600

    REALEST-ESTATE: Install some packages and add ourter in app

--- django/realest_estate/frontend/src/App.js
+++ django/realest_estate/frontend/src/App.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import About from './containers/About';
+import Contact from './containers/Contact';
+import Listing from './containers/Listings';
+import ListingDetail from './containers/ListingDetail';
+import LogIn from './containers/LogIn';
+import SignUp from './containers/SignUp';
+import Layout from './hocs/Layout';
 
-function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
-}
+const App = () => {
+  <Router>
+    <Layout>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/listing' component={Listing} />
+        <Route exact path='/listing/:id' component={ListingDetail} />
+        <Route exact path='/login' component={LogIn} />
+        <Route exact path='/signup' component={SignUp} />
+      </Switch>
+    </Layout>
+  </Router>
+};
 
 export default App;
