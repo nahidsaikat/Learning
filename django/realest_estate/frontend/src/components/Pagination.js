e3d0c22cf480b2d05a82c48e4e8bf26cdad4cd1c
nahidsaikat <nahidsaikatft40@gmail.com>
2021-06-28 22:46:46 +0600

    REALEST-ESTATE: Making home page working without pagination

---
+++ django/realest_estate/frontend/src/components/Pagination.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const pagination = () => (
+    <div>
+        pagination
+    </div>
+);
+
+export default pagination;
--- django/realest_estate/frontend/src/containers/Home.js
+++ django/realest_estate/frontend/src/containers/Home.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { setAlert } from '../actions/alert';
+import ListingForm from '../components/ListingForm';
+import Listings from '../components/Listings';
+import Pagination from '../components/Pagination';;
 
-const home = () => (
-    <div>
-        Home
-    </div>
-);
+const Home = () => {
+    const [listings, setListings] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [listingsPerPage, setListingsPerPage] = useState(3);
+    const [active, setActive] = useState(1);
 
-export default home;
+    const indexOfLastListing = currentPage * listingsPerPage;
+    const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+    const currentListings = listings.slice(indexOfFirstListing, indexOfLastListing);
+
+    const visitPage = (page) => {
+        setCurrentPage(page);
+        setActive(page);
+    };
+
+    const previousNumber = () => {
+        if (currentPage !== 1) {
+            setCurrentPage(currentPage-1);
+            setActive(currentPage-1);
+        }
+    };
+
+    const nextNumber = () => {
+        if (currentPage !== Math.ceil(listings.length/3)) {
+            setCurrentPage(currentPage+1);
+            setActive(currentPage+1);
+        }
+    };
+
+    return (
+        <main className='home'>
+            <Helmet>
+                <tilte>Realest Estate - Home</tilte>
+                <meta 
+                    name='description'
+                    content='Realest Estate Home Page'
+                />
+            </Helmet>
+            <section className='home__form'>
+                <ListingForm setListings={setListings} />
+            </section>
+            <section className='home__listings'>
+                <Listings listings={currentListings} />
+            </section>
+            <section className='home__pagination'>
+                <div className='row'>
+                    {
+                        listings.length !== 0 ? (
+                            <Pagination
+                                itemsPerPage={listingsPerPage}
+                                count={listings.length}
+                                visitPage={visitPage}
+                                previous={previousNumber}
+                                next={nextNumber}
+                                action={active}
+                                setActive={setActive}
+                            />
+                        ) : null
+                    }
+                </div>
+            </section>
+        </main>
+    );
+};
+
+export default Home;
