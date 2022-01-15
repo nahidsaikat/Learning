01c9a2f6e0d479eaa4ac30ec97b2319abe8ec337
nahidsaikat <nahidsaikatft40@gmail.com>
2022-01-15 20:52:46 +0600

    Add values.go file

---
+++ go/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 1+1)
+	fmt.Println("2.0/3.0=", 2.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(false || true)
+	fmt.Println(!true)
+}
