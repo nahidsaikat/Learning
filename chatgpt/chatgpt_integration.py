693c49f73beaac54e9b496ed4c4aeb0a78ceb752
nahid_martailer <nahid@martailer.com>
2024-11-29 18:37:58 +0600

    Add chatgpt python integration example

---
+++ chatgpt/chatgpt_integration.py
@@ -0,0 +1,20 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
+
+# Extract and print the response
+# print(chat_completion['choices'][0]['message']['content'])
+print(chat_completion)
